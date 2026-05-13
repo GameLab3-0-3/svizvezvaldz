fe799516e0d119fed2bf50f0413779cdf2d48fd9
Deva <manuelbocelli1@gmail.com>
2026-05-13 09:19:52 +0200

    movement di base

---
+++ Assets/Scripts/Manuel/Movement/Movement.cs
@@ -0,0 +1,42 @@
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    InputMap inputActions;
+    Rigidbody rb;
+    [SerializeField] float speed;
+    public static Movement instance;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        inputActions = new InputMap();
+        if(instance != null)
+        {
+            Destroy(instance);
+            return;
+        }
+            instance = this;
+    }
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+    void FixedUpdate()
+    {
+        if(inputActions.Player.Movement.ReadValue<Vector2>() != Vector2.zero)
+        {
+            Movementfunction();
+        }
+    }
+    private void Movementfunction()
+    {
+        Vector2 movementInput = inputActions.Player.Movement.ReadValue<Vector2>(); 
+        Vector3 movement = rb.position + new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.fixedDeltaTime;
+        rb.position = movement;
+    }
+}
