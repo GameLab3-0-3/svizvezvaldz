49908180c6a492e0c9ba3a1377a09f60d7031ca4
Loris Arrigo <loris.arrigo@gmail.com>
2026-05-20 13:21:46 +0100

    anomaly chooser 1
    aggiunta una trigger zone aggiuntiva che permette di far scegliere se è presente o meno un'anomalia;
    sistemata la loop logic di conseguenza.

---
+++ Assets/Scripts/Loris/Anomaly_Chooser.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Anomaly_Chooser : MonoBehaviour
+{
+    [Header("Riferimenti")]
+    [Tooltip("Qui mettiamo il Player per teletrasportarlo")]
+    public GameObject Player;
+
+    [Tooltip("Qui passiamo la posizione per teletrasportare il PLayer")]
+    public Transform destination;
+
+    [Tooltip("Distanza di sicurezza per evitare che il Player venga loopato dentro le TriggerZone")]
+    public float tpOffset;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            if (GameManager.instance.triggerCounter == 0)
+            {
+                int anomaly = Random.Range(0, 101);
+                Debug.Log(anomaly);
+                if (anomaly <= 50)
+                    GameManager.instance.anomaly = false;
+                else if (anomaly > 50 && anomaly <= 100)
+                    GameManager.instance.anomaly = true;
+            }
+            GameManager.instance.triggerCounter++;
+            Vector3 relativePos = Player.transform.position - transform.position;
+
+            Vector3 destinationPos = destination.position + new Vector3(0, relativePos.y, relativePos.z);
+
+            if (GameManager.instance.triggerCounter >= 2)
+            {
+                destinationPos.x += tpOffset;
+                GameManager.instance.UpdateChooser();
+                Player.transform.position = destinationPos;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,15 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [Header("Exit")]
+    [Tooltip("Questo è il trigger d'entarta, quello che ti teletrasporta alla fine del corridoio precedente")]
     public GameObject enterTrigger;
+    [Tooltip("Questo è il trigger d'uscita, quello che ti teletrasporta all'inizio del corridoio successivo")]
     public GameObject exitTrigger;
+    [Tooltip("Questo è il trigger che decide se andare avanti o dietro, attivva/disattiva il bool, decide quale anomalia deve esserci (se il bool è attivo) e ti teletrasporta all'entrata se torni indietro")]
+    public GameObject anomalyChooser;
+    [Tooltip("bool utilizzato per far sapere al gioco se o meno nel corridoio ci sono anomalie")]
     public bool anomaly;
 
     [Header("Counter")]
+    [Tooltip("il numero massimo di volte che il Player deve raggiungere per finire il gioco")]
     public float maxCounter;
+    [Tooltip("Counter che aumenta nel caso in cui andiamo nella direzione giusta, si resetta se sbagliamo, e disattiva il trigger in fondo per permettere al giocatore di finire il gioco")]
     public float counter;
+    [Tooltip("Counter necessario per sapere quante volte il Player è entrato nell'Anomaly Chooser in modo da far funzionare il Game loop")]
+    public int triggerCounter;
 
     public static GameManager instance;
     private void Awake()
@@ -21,9 +31,39 @@ public class GameManager : MonoBehaviour
         }
         instance = this;
     }
-    #region RriggerZones counter
+    #region TriggerZones counter
+    //questa funzione viene richiamata in "Anomaly_Checks" nel trigger d'uscita per far funzionare il game Loop
+    //questa funzione viene messa in "Anomaly_Checks" nel trigger d'entrata per creare una sorta di loop
+    public void Loop()
+    {
+        //se il counter è maggiore di 0 e non è presente nessuna anomalia il counter scende di uno, impedendo al Player di sfruttare il tp come metodo di fine veloce, sostanzialmente creando di fatto il loop
+        if (counter > 0 && !anomaly)
+            counter--;
+        //altrimenti se è presente un'anomalia il counter aumenta (questa parte è da spostare nel trigger "anomalyChooser" per finalizzare il game loop)
+        //else if (counter > 0 && anomaly)
+        //    counter++;
+        //questa ultimo controllo è per evitare, un'altra volta, che il Player possa sfruttare il tp come metodo veloce per finire il gioco. infatti se volesse ritornare indietro, il Player tornerebbe alla fine del corridoio precedente e il trigger d'uscita si riattiverebbe impedendo di andare alla fine senza aver percorso l'ultimo corridoio
+        if (counter < maxCounter)
+            exitTrigger.SetActive(true);
+    }
+    public void UpdateChooser()
+    {
+        //se sono presenti anomalie il trigger ti aumenta il counter e nel caso in cui il counter raggiunge il massimo disattiva l'uscita
+        if (anomaly)
+        {
+            StartCoroutine(CorridorCounter());
+        }
+        //se non è presente un'anomalia resetta il counter (skill issue negro)
+        else
+        {
+            counter = 0;
+        }
+        triggerCounter = 0;
+    }
     public void UpdateCounter()
     {
+        triggerCounter = 0;
+        //se non sono presenti anomalie il trigger ti aumenta il counter e nel caso in cui il counter raggiunge il massimo disattiva l'uscita
         if (!anomaly)
         {
             counter++;
@@ -31,29 +71,22 @@ public class GameManager : MonoBehaviour
             if (counter == maxCounter)
                 exitTrigger.SetActive(false);
         }
+        //se è presente un'anomalia resetta il counter (skill issue negro)
         else
         {
             counter = 0;
         }
-    }
-    public void Loop()
-    {
-        if (counter > 0 && !anomaly)
-            counter--;
-
-        else if (counter > 0 && anomaly)
-            counter++;
-
-        if (counter < maxCounter)
-            exitTrigger.SetActive(true);
+        anomaly = false;
     }
 
-    private void Update()
+    IEnumerator CorridorCounter()
     {
-        if (counter == 0)
-            enterTrigger.SetActive(false);
-        else if (counter > 0)
-            enterTrigger.SetActive(true);
+        counter++;
+        if (counter > maxCounter) counter = maxCounter;
+        if (counter == maxCounter)
+            exitTrigger.SetActive(false);
+        yield return new WaitForSeconds(1);
+        anomaly = false;
     }
     #endregion
 }
