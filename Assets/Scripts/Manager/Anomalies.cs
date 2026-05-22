92e70585f7d10b062ca3cedc8329ec5a27bd0e52
Loris Arrigo <loris.arrigo@gmail.com>
2026-05-22 20:30:36 +0100

    Anomaly 2
    - implementati altri poster;
    - Seconda anomalia: dimensioni poster modificate;

--- Assets/Scripts/Manager/Anomalies.cs
+++ Assets/Scripts/Manager/Anomalies.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Anomalies : MonoBehaviour
 {
-    public GameObject Poster2Normal;
-    public GameObject Poster2ALt;
-    public GameObject Poster4Normal;
-    public GameObject Poster4Alt;
+    [Header("Posters")]
+    public GameObject Poster2_Normal;
+    public GameObject Poster4_Normal;
+    public GameObject PosterSet_Normal;
+    [Header("----------")]
+    public GameObject Poster2_ALt;
+    public GameObject Poster4_Alt;
+    public GameObject PosterSet_Alt;
+
     public static Anomalies instance;
     private void Awake()
     {
@@ -38,22 +43,22 @@ public class Anomalies : MonoBehaviour
         {
             GameManager.instance.anomaly = true;
             float type = Random.Range(0, 21);
-            if (type <= 10)
+            if (type <= 6.66f)
             {
                 Poster2();
                 return;
             }
-            else if (type > 10 && type <21)
+            else if (type > 6.66f && type <= 13.33f)
             {
                 Poster4();
                 return;
             }
-            /*
-            else if (type == 2)
+            else if (type > 13.33f && type < 21)
             {
-                Debug.Log("poster grande");
+                GigaPoster();
                 return;
             }
+            /*
             else if (type == 3)
             {
                 Debug.Log("poster occhi");
@@ -150,24 +155,33 @@ public class Anomalies : MonoBehaviour
 
     private void Poster2()
     {
-        Poster2Normal.SetActive(false);
-        Poster2ALt.SetActive(true);
+        Poster2_Normal.SetActive(false);
+        Poster2_ALt.SetActive(true);
     }
     private void Poster4()
     {
-        Poster4Normal.SetActive(false);
-        Poster4Alt.SetActive(true);
+        Poster4_Normal.SetActive(false);
+        Poster4_Alt.SetActive(true);
+    }
+
+    private void GigaPoster()
+    {
+        PosterSet_Normal.SetActive(false);
+        PosterSet_Alt.SetActive(true);
     }
     #endregion Anomalies
     private void ResetAlt()
     {
         #region poster2/4
         //poster 2
-        Poster2Normal.SetActive(true);
-        Poster2ALt.SetActive(false);
+        Poster2_Normal.SetActive(true);
+        Poster2_ALt.SetActive(false);
         //poster 4
-        Poster4Normal.SetActive(true);
-        Poster4Alt.SetActive(false);
+        Poster4_Normal.SetActive(true);
+        Poster4_Alt.SetActive(false);
+        //poster Set
+        PosterSet_Normal.SetActive(true);
+        PosterSet_Alt.SetActive(false);
         #endregion
     }
 }
